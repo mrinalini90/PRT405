4c4b969718e5c21b0893fcd0d9ffedbf0554560a
Meeru <mirupikachu@gmail.com>
2017-04-25 20:36:47 +0930

    Angular Package added and Create Table sql code added

---
+++ Catalogue/Database/CreateTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Users(
+       UserID   int   NOT NULL unique IDENTITY(1,1),
+       UserName Varchar (20)     NOT NULL unique,
+       UserPasssword   varchar(40)  NOT NULL,
+       UserRole     Varchar(20)  NOT NULL,
+       PRIMARY KEY (UserID)
+);
+CREATE TABLE Items  
+(  
+ ItemID int IDENTITY(1,1) unique,  
+ ItemName varchar (20),  
+ ItemDescription char(1),  
+ ItemPrice varchar(30) ,
+ UserID int FOREIGN KEY REFERENCES Users(UserID),
+  PRIMARY KEY (ItemID) 
+); 
